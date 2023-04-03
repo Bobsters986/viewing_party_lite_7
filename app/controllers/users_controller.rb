af6346a5d0d7063324db40726ed43bbabbf6b481
Bobby Luly <bobbyluly@gmail.com>
2023-04-03 17:50:21 -0600

    Add session_id to create and login_user actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,13 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
       flash[:success] = "User successfully created"
-      redirect_to "/users/#{@user.id}"
+      redirect_to "/users/#{user.id}"
     else
-      flash[:error] = error_message(@user.errors)
+      flash[:error] = error_message(user.errors)
       redirect_to "/register"
     end
   end
