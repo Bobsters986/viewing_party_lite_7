9a1b81bfb07d1bca7f21d82bed6c9d818a15ff82
Bobby Luly <bobbyluly@gmail.com>
2023-04-03 14:48:43 -0600

    Refactor: adding password attribute to a user, add valisations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-  validates_presence_of :name, :email
-  validates_uniqueness_of :email
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates_presence_of :password
+  has_secure_password
 
   has_many :user_parties
   has_many :parties, through: :user_parties
