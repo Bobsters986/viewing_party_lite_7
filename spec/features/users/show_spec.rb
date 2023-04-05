0b854e7e92671185b6e76b4b1bbe761ea48de9d4
Bobby Luly <bobbyluly@gmail.com>
2023-04-04 19:48:00 -0600

    Refactor all tests involving vcrs

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe "User Registration", type: :feature do
+describe "User Registration", :vcr do
   before(:each) do
     @stan = User.create!(name: "Stan Johnson", email: "stan@example.com", password: "test123")
     @ben = create(:user, password: "test124")
@@ -20,69 +20,60 @@ RSpec.describe "User Registration", type: :feature do
     UserParty.create!(user_id: @sarah.id, party_id: @their_party.id)
     UserParty.create!(user_id: @jill.id, party_id: @their_party.id)
 
-    json_response = File.read('spec/fixtures/fight_club_info.json')
-      stub_request(:get, "https://api.themoviedb.org/3/movie/550?api_key=108e8ef231bf49bc3de4db0a0c14366c").
-        to_return(status: 200, body: json_response)
-
-    visit root_path
-
-    click_link "Log In"
-
+    visit login_path
     fill_in :email, with: @stan.email
     fill_in :password, with: @stan.password
-
-    click_on "Log In"
+    click_button "Log In"
   end
 
-  describe "User Story 6" do
-    describe "As a user, when I visit a users dashboard page(/users/:id})" do
-      it "I should see the name of the user's dashboard" do
-        save_and_open_page
-        expect(page).to have_content("#{@stan.name}'s Dashboard")
-      end
+  context "US6 As a user, when I visit a users dashboard page" do
+    it "I should see the name of the user's dashboard" do
+      
+      expect(page).to have_content("#{@stan.name}'s Dashboard")
+    end
 
-      it "has a button to 'Discover Movies'" do
-        expect(page).to have_button "Discover Movies"
-      end
+    it "has a button to 'Discover Movies'" do
+      expect(page).to have_button "Discover Movies"
+    end
 
-      it "has a section that shows all viewing parties" do
-        within "#viewing_parties" do
-          expect(page).to have_content("Viewing Parties")
+    it "has a section that shows all viewing parties" do
+      within "#viewing_parties" do
+        expect(page).to have_content("Viewing Parties")
+      end
+    end
+
+    it "when I click the 'Discover Movies' button, I am routed to the discover page" do
+      click_button "Discover Movies"
+
+      expect(current_path).to eq("/users/#{@stan.id}/discover")
+    end
+  end
+
+  context "US8 As a user, when I visit a users dashboard page" do
+    it "I will see my viewing parties details, details of viewing parties I am invited to, and the movie title and img" do
+      within "#party-#{@my_party.id}" do
+        expect(page).to have_css("img")
+        expect(page).to have_link("Fight Club")
+        expect(page).to have_content("Date: March 26, 2023")
+        expect(page).to have_content("Time: 08:00 PM")
+        expect(page).to have_content("Host: #{@stan.name}")
+        within "ul#guests" do
+          expect(page).to have_content("#{@ben.name}")
+          expect(page).to have_content("#{@sarah.name}")
+          expect(page).to have_content("#{@jill.name}")
         end
       end
 
-      it "when I click the 'Discover Movies' button, I am routed to the discover page" do
-        click_button "Discover Movies"
-
-        expect(current_path).to eq("/users/#{@stan.id}/discover")
-      end
-
-      it "I will see my viewing parties details, details of viewing parties I am invited to, and the movie title and img" do
-        
-        within "#party-#{@my_party.id}" do
-          expect(page).to have_css("img")
-          expect(page).to have_link("Fight Club")
-          expect(page).to have_content("Date: March 26, 2023")
-          expect(page).to have_content("Time: 08:00 PM")
-          expect(page).to have_content("Host: #{@stan.name}")
-          within "ul#guests" do
-            expect(page).to have_content("#{@ben.name}")
-            expect(page).to have_content("#{@sarah.name}")
-            expect(page).to have_content("#{@jill.name}")
-          end
-        end
-
-        within "#party-#{@their_party.id}" do
-          expect(page).to have_css("img")
-          expect(page).to have_link("Fight Club")
-          expect(page).to have_content("Date: March 31, 2023")
-          expect(page).to have_content("Time: 07:00 PM")
-          expect(page).to have_content("Host: #{@ben.name}")
-          within "ul#guests" do
-            expect(page).to have_content("#{@stan.name}")
-            expect(page).to have_content("#{@sarah.name}")
-            expect(page).to have_content("#{@jill.name}")
-          end
+      within "#party-#{@their_party.id}" do
+        expect(page).to have_css("img")
+        expect(page).to have_link("Fight Club")
+        expect(page).to have_content("Date: March 31, 2023")
+        expect(page).to have_content("Time: 07:00 PM")
+        expect(page).to have_content("Host: #{@ben.name}")
+        within "ul#guests" do
+          expect(page).to have_content("#{@stan.name}")
+          expect(page).to have_content("#{@sarah.name}")
+          expect(page).to have_content("#{@jill.name}")
         end
       end
     end
--- spec/features/visitors/movie_show_spec.rb
+++ spec/features/visitors/movie_show_spec.rb
@@ -1,30 +1,17 @@
 require "rails_helper"
 
-RSpec.describe "Visitor Movie Show", type: :feature do
+describe "Visitor Authorization Movie Show", :vcr do
   before(:each) do
     #visitor = user who's not logged in
     @user_1 = create(:user, password: "test123")
-
-    VCR.eject_cassette(:movie_by_id_info)
-    VCR.eject_cassette(:cast_info)
-    VCR.eject_cassette(:review_info)
+    visit user_movie_path(@user_1, 550) #Fight Club id is 550
   end
 
-  describe "Visitor Authorization" do
-    it "cant create a viewing party if not logged in" do
-      VCR.insert_cassette(:movie_by_id_info, serialize_with: :json) do
-        VCR.insert_cassette(:cast_info, serialize_with: :json) do
-          VCR.insert_cassette(:review_info, serialize_with: :json) do
-            visit user_movie_path(@user_1, 550) #Fight Club id is 550
-            
-          end
-        end
-      end
-      save_and_open_page
-      click_button "Create Viewing Party for Fight Club"
-      expect(page).to have_content("You must be logged in or registered to view this page")
-      expect(current_path).to eq(root_path)
-    end
+  it "cant create a viewing party if not logged in" do
+    click_button "Create Viewing Party for Fight Club"
+    
+    expect(page).to have_content("You must be logged in or registered to create a viewing party")
+    expect(current_path).to eq(user_movie_path(@user_1, 550))
   end
 end
 
