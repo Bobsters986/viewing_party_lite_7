623cd800e05c617450650854b692fd6849b67343
Bobby Luly <bobbyluly@gmail.com>
2023-04-05 11:46:44 -0600

    Add login to log out test

--- spec/features/users/user_can_log_out_spec.rb
+++ spec/features/users/user_can_log_out_spec.rb
@@ -3,20 +3,10 @@ require 'rails_helper'
 RSpec.describe "Logging In" do
   it "can log in with valid credentials" do
     user = User.create!(name: "Bob Johnson", email: "bob@example.com", password: "test123")
-
-    visit root_path
-
-    click_link "Log In"
-
-    fill_in :email, with: user.email
-    fill_in :password, with: user.password
-
-    click_on "Log In"
+    log_in(user)
 
     expect(current_path).to eq("/dashboard")
     
-    # allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-
     click_link "Home"
 
     expect(page).to_not have_link("Log In")
